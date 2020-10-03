619e45e757b956b7011ee14d58421640766c6140
Islam <doctorr01@yandex.ru>
2020-10-03 19:33:40 +0600

    poh

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array.length === 0 ) {
+    return 0;
+  }
+  return Math.min.apply(null, array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array.length === 0 ) {
+    return 0;
+  }
+  return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array.length === 0) {
+    return 0;
+}
+let value = array[0];
+for(let i=1;i < array.length;i++) {
+    value += array[i];
+}
+return value/array.length;
 }
